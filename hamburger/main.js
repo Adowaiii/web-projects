b37bb860f4b9f1da019e7199c06a39ac46cab427
JuliaChen4061 <50419julia@gmail.com>
2023-01-18 13:28:01 +0800

    Hamburger done

--- hamburger/main.js
+++ hamburger/main.js
@@ -1,14 +1,15 @@
 
+const buttons = document.querySelectorAll("button");
 
-
-const buttonOne = document.querySelector(".button-one");
-
-buttonOne.addEventListener('click', () =>{
-    const isOpened = buttonOne.getAttribute('aria-expanded');
-    console.log(isOpened);
-    if (isOpened === 'false'){
-        buttonOne.setAttribute('aria-expanded', 'true');
-    }else{
-        buttonOne.setAttribute('aria-expanded', 'false');
-    }
+buttons.forEach((button) => {
+    button.addEventListener('click', () =>{
+        const currentState = button.getAttribute("data-state");
+        if (!currentState || currentState === 'closed'){
+            button.setAttribute('data-state', 'opened');
+            button.setAttribute('aria-expanded', 'true');
+        }else{
+            button.setAttribute('data-state', 'closed');
+            button.setAttribute('aria-expanded', 'false');
+        }
+    });
 });
